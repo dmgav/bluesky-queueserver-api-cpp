a2b1807aeeffdd06a3a1ae49e3c72b3869e36669
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-10-07 21:45:53 -0400

    CI: use clang-format-14

--- clang_validate.sh
+++ clang_validate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find ./code -regextype posix-egrep -regex "\./code/(src|tests|include)/.*\.(h|hpp|c|cc|cpp)?" \
-    | xargs clang-format -i --verbose --Werror --dry-run
+    | xargs clang-format-14 -i --verbose --Werror --dry-run
